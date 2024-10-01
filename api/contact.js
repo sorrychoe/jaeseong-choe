a3d4fd4ffc8b0262ad186eae78d2e95fcdabf470
sorrychoe <cjssoote@gmail.com>
2024-10-01 22:32:56 +0900

    set secure port

--- api/contact.js
+++ api/contact.js
@@ -33,7 +33,7 @@ module.exports = async (req, res) => {
         transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT,
-            secure: process.env.SMTP_PORT == 465,
+            secure: process.env.SMTP_SECURE === 'true',
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS,
