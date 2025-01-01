34c321831db73a851dbe562bc9cd1f30d4e6ca0e
sorrychoe <cjssoote@gmail.com>
2025-01-01 20:48:11 +0900

    text change

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -18,16 +18,13 @@ function Education() {
                 <br/>
                 <p>As a student majored in Media & Communication Studies, I studied Mass Media theory & the Cultural Studies.</p> 
                 <p>In particular, I studied with a focus on analyzing socio-cultural phenomena reproduced by the mass media. </p>
-                {/* <p>I wrote a academic article about analyze Korean media tends to stigmatize people with mental illness.</p> */}
+                <p>I wrote a academic article about analyze Korean media tends to stigmatize people with mental illness.</p>
             </div>
             <div class="education-item">
                 <h3>Bachelor of Science in Data Science</h3>
                 <p>Handong Global University | 2019 - 2025</p>
                 <br/>
                 <p>I studied Text Mining & Natural Language Processing with a focus.</p>
-                {/* <p>As a Capstone project, I have developed an Chatbots for smart farm user.</p> */}
-                <p>Futhermore, I was explored mathematics such as Linear Algebra, Probability Theory & Mathematical Statistics.</p>
-                <p>Especially, I was interested in statistical method such as Regression Analysis & Cluster Analysis.</p>
             </div>
         </div>
     </section>
