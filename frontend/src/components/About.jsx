b4a5506333b738d90f4202e8aced73c83743cc7d
sorrychoe <cjssoote@gmail.com>
2025-05-07 16:13:10 +0900

    Updated

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -13,10 +13,9 @@ function About() {
     <section id="about" className="py-5 bg-light" data-aos="fade-up">
       <div className="container">
         <h2>About Me</h2>
-        <p align="left">Specialist of social data analysis with extensive experience in Human Resource Development, Project Management, and Market Research</p>
-        <p aline="left">Skilled in transforming complex data into actionable insights through advanced analytics, data visualization, and machine learning.</p>
+        <p align="left">Specialist of social data analysis with extensive experience in Media Research & Human Resource Development</p>
+        <p aline="left">Skilled in transforming complex data into actionable insights through statistical analytics, data visualization, and machine learning.</p>
         <p align="left">Proven track record in managing projects and improving processes with a data-driven approach.</p>
-        <p align="left">Strong communicator and problem solver with hands-on experience in Project Management and Data Analysis.</p>
       </div>
     </section>
   );
