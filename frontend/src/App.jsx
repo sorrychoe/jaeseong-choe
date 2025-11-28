a8a517c1d46354aac926e8b741749197327db98b
sorrychoe <cjssoote@gmail.com>
2025-11-29 00:09:02 +0900

    Modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Software from './components/Software';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
