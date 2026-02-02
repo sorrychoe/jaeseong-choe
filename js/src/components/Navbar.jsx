afc0c48f3b82277b8f0cb51d8de99a013c1661e8
sorrychoe <cjssoote@gmail.com>
2026-02-02 20:54:19 +0900

    Typo

--- js/src/components/Navbar.jsx
+++ js/src/components/Navbar.jsx
@@ -6,12 +6,12 @@ function Navbar() {
     <nav className={styles.nav}>
       <ul className={styles.list}>
         <li><a href="#about" className={styles.link}>About me</a></li>
-        <li><a href="#experience" className={styles.link}>Experience</a></li>
+        <li><a href="#experience" className={styles.link}>Experiences</a></li>
         <li><a href="#education" className={styles.link}>Education</a></li>
         <li><a href="#skills" className={styles.link}>Skills</a></li>
         <li><a href="#projects" className={styles.link}>Projects</a></li>
-        <li><a href="#publication" className={styles.link}>Publication</a></li>
-        <li><a href="#softwares" className={styles.link}>Software</a></li>
+        <li><a href="#publication" className={styles.link}>Publications</a></li>
+        <li><a href="#softwares" className={styles.link}>Softwares</a></li>
         <li><a href="#contact" className={styles.link}>Contact</a></li>
       </ul>
     </nav>
