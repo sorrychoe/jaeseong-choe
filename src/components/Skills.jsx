f4ef969f35369c7dd622f461f00cd158d0564873
sorrychoe <cjssoote@gmail.com>
2026-04-01 01:05:38 +0900

    Organize Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,28 +11,28 @@ function Skills() {
     { name: 'R', icon: 'https://cdn.simpleicons.org/R/FFFFFF' },
     { name: 'Python', icon: 'https://cdn.simpleicons.org/python/FFFFFF' },
     { name: 'Julia', icon: 'https://cdn.simpleicons.org/Julia/FFFFFF' },
+    { name: 'SQL', icon: 'https://cdn.simpleicons.org/sqlite/FFFFFF' },
     { name: 'JavaScript', icon: 'https://cdn.simpleicons.org/JavaScript/FFFFFF' },
-    { name: 'HTML', icon: 'https://cdn.simpleicons.org/HTML5/FFFFFF' },
-    { name: 'CSS', icon: 'https://cdn.simpleicons.org/CSS/FFFFFF' },
+    { name: 'Node.js', icon: 'https://cdn.simpleicons.org/node.js/FFFFFF' },
 
     { name: 'Tidyverse', icon: 'https://cdn.simpleicons.org/Tidyverse/FFFFFF' },
-    { name: 'Pandas', icon: 'https://cdn.simpleicons.org/pandas/FFFFFF' },
+    { name: 'Scikit-Learn', icon: 'https://cdn.simpleicons.org/scikitlearn/FFFFFF' },
     { name: 'Pytorch', icon: 'https://cdn.simpleicons.org/pytorch/FFFFFF' },
-    { name: 'Selenium', icon: 'https://cdn.simpleicons.org/Selenium/FFFFFF' },
     { name: 'PySpark', icon: 'https://cdn.simpleicons.org/apachespark/FFFFFF' },
     { name: 'Airflow', icon: 'https://cdn.simpleicons.org/apacheairflow/FFFFFF' },
-
-    { name: 'Anaconda', icon: 'https://cdn.simpleicons.org/anaconda/FFFFFF' },
-    { name: 'Git', icon: 'https://cdn.simpleicons.org/Git/FFFFFF' },
-    { name: 'Linux', icon: 'https://cdn.simpleicons.org/gnubash/FFFFFF' },
-    { name: 'Docker', icon: 'https://cdn.simpleicons.org/Docker/FFFFFF' },
+    { name: 'Selenium', icon: 'https://cdn.simpleicons.org/Selenium/FFFFFF' },
     { name: 'Databricks', icon: 'https://cdn.simpleicons.org/databricks/FFFFFF' },
     { name: 'Redash', icon: 'https://cdn.simpleicons.org/redash/FFFFFF' },
-
+    { name: 'Docker', icon: 'https://cdn.simpleicons.org/Docker/FFFFFF' },
+    { name: 'Git', icon: 'https://cdn.simpleicons.org/Git/FFFFFF' },
+    { name: 'Anaconda', icon: 'https://cdn.simpleicons.org/anaconda/FFFFFF' },
+    { name: 'Linux', icon: 'https://cdn.simpleicons.org/gnubash/FFFFFF' },
     { name: 'Jupyter', icon: 'https://cdn.simpleicons.org/jupyter/FFFFFF' },
     { name: 'Quarto', icon: 'https://cdn.simpleicons.org/Quarto/FFFFFF' },
     { name: 'Markdown', icon: 'https://cdn.simpleicons.org/Markdown/FFFFFF' },
     { name: 'Latex', icon: 'https://cdn.simpleicons.org/Latex/FFFFFF' },
+    { name: 'HTML', icon: 'https://cdn.simpleicons.org/HTML5/FFFFFF' },
+    { name: 'CSS', icon: 'https://cdn.simpleicons.org/CSS/FFFFFF' },
   ];
 
   return (
