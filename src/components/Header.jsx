0d4b6436b4e40af2d0c8d9c8b4b697a2b8c9b568
sorrychoe <cjssoote@gmail.com>
2026-09-12 03:39:31 +0900

    Change Header Message

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
           Jaeseong Choe
         </h1>
         <p className={styles.subtitle}>
-          Opinion Miner | Data Analyst | Computational Communication Researcher
+          Data Analyst | Marketing & People Analytics | Computational Social Science
         </p>
         <Navbar />
       </div>
